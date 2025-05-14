973d61fa737007bb220a258ff98e8e2d8b6388bf
GG-Pharaoh <oxdiablo20@gmail.com>
2025-05-14 07:56:13 -0700

    All tested

--- src/index.ts
+++ src/index.ts
@@ -98,3 +98,69 @@ const user1: UserInterface = {
 };
 
 
+interface MathFunc {
+    (x: number, y: number): number
+}  
+
+const add: MathFunc = (x: number, y: number): number => x + y 
+const sub: MathFunc = (x: number, y: number): number => x - y 
+
+
+interface PersonInterface {
+    id: number
+    name: string
+    register(): string
+}
+
+
+//Classes 
+class Person implements PersonInterface {
+    id: number 
+    name: string 
+
+    constructor(id: number, name: string) {
+        this.id = id 
+        this.name = name 
+
+    }
+
+    register() {
+    return `${this.name} is now registered`
+ }
+
+}
+
+ 
+
+const brad = new Person(1, "Brad Traversy")
+const mike = new Person(2, "Mike Smith")
+
+
+
+//Subclasses
+class Employee extends Person {
+    position: string
+
+    constructor(id: number, name: string, position: string) {
+        super(id, name)
+        this.position = position
+    }
+}
+
+
+const emp = new Employee(3, 'Shawn', 'Developer')
+
+console.log(emp.name)
+console.log(emp.register())
+
+
+
+//Generics 
+function  getArray<T>(items:  T []) : T [] {
+    return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1,2,3,4])
+let strArray = getArray<string>(['Brad', 'John', 'Jill'])
+
+strArray.push
